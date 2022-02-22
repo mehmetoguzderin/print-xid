0cd593c2b8e2e8b7f1ba4d290e5187ac09b56445
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2022-02-22 13:48:14 -0800

    Update.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,19 @@ use std::fs;
 
 use unicode_xid::UnicodeXID;
 
+#[test]
+fn trojan_source() {
+    let mut vulnerable = false;
+    let paper_codepoints =
+        "\u{202A}\u{202B}\u{202D}\u{202E}\u{2066}\u{2067}\u{2068}\u{202C}\u{2069}";
+    for ch in paper_codepoints.chars() {
+        if UnicodeXID::is_xid_start(ch) || UnicodeXID::is_xid_continue(ch) {
+            vulnerable = true;
+        }
+    }
+    assert_eq!(vulnerable, false);
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
